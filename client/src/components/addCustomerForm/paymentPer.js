f7468a14f691a5760f0f5979670e37a67c06404e
khaled <thenonexist@hotmail.com>
2020-06-03 17:09:16 +0300

    update addcustomer

--- client/src/components/addCustomerForm/paymentPer.js
+++ client/src/components/addCustomerForm/paymentPer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const paymentPer = (props) => {
       return (
         <div>
-          <label style={{color:'white'}}> Every </label>
+          <label style={{color:'white'}}> Every </label> <br></br>
           <select 
           type="number"
-          className="input-reset ba b--black-20 pa2 br3 mb2 ma1 w-30"
+          className="input-reset ba b--black-20 pa2 br3 mb2 ma1 w-100"
           onChange={(e, id) => props.onChange(e, props.stateId)}>
               <option value="Appointment">Appointment</option>
               <option value="Week">Week</option>

032776bd1a4b06a29eef179011931b51f134ac57
fridabu <f0504070002@gmail.com>
2020-06-04 13:00:42 +0300

    change customer note field to global component

--- client/src/components/addCustomerForm/customerForm.js
+++ client/src/components/addCustomerForm/customerForm.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import CustomerName from './customerName';
 import CustomerEmail from './customerEmail';
-import CustomerNote from './customerNote';
 import CustomerPhone from './customerPhone';
 import CustomerPrice from './customerPrice';
 import PaymentNumber from './paymentEvery';
 import PaymentPeriod from './paymentPer';
+import NoteFieldText from '../NoteFieldText';
 import { addCustomer } from '../../actions/customers';
 import { Redirect } from 'react-router'
 
@@ -82,7 +82,7 @@ function CustomerForm() {
             placeHolder={customerDetails.paymentEveryUnit}
           />
         </div>
-        <CustomerNote
+        <NoteFieldText
           stateId="notes"
           onChange={handleChange}
           placeHolder={customerDetails.notes}

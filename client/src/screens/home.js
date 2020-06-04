e0c6841c207e6c7ceae967ef39131822363129c0
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-04 11:09:39 +0300

    partial work on delete popups

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import ScreenContainer from '../components/Screen';
+import ConfirmDeleteAppointment from '../components/ConfirmDeleteAppointment'
 
 const Home = () => {
 
@@ -15,6 +16,7 @@ const Home = () => {
     return (
         <ScreenContainer>
             <h1>Home Page</h1>
+            <ConfirmDeleteAppointment/>
             <Calendar formatLongDate={(locale, date) => date.toLocaleDateString()} onChange={onChange}
                 value={date}
                  />

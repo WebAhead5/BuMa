712a37479ae356c306fe0913a20ab3b668d8514e
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-07 14:47:14 +0300

    add style for popup, and made the exit button appear depending on the number of the props.labels sent through the father component of the popup.

--- client/src/components/Popups/Popup.js
+++ client/src/components/Popups/Popup.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+const styles = {
+    closeBtn: {
+        border: 'none',
+        backgroundColor: '#0B8D98',
+        textDecoration: 'none',
+        textAlign: 'center',
+    }
+}
+
 const Popup = (props) => {
     // buttons options
     const buttonsNumber = props.labels;
@@ -8,23 +17,28 @@ const Popup = (props) => {
     const callbacks = props.callbacks;
 
     // handle errors
-    if(buttonsNumber.length !== callbacks.length) {
+    if (buttonsNumber.length !== callbacks.length) {
         throw new Error('buttonsNumber length does not match callbacks length')
     }
-  
+
     // populate div with buttons according to props.labels length.
-   const buttons = buttonsNumber.map((button, index) => (
-        <div>
-        <button onClick={callbacks[index]}>{button}</button>
-        </div>
+    const buttons = buttonsNumber.map((button, index) => (
+        <button style={props.style} onClick={callbacks[index]}>{button}</button>
     ))
 
     // return the Components to be used on other components
     return (
-        <Modal isOpen={props.isOpen} style={props.style} >
-            <button onClick={() => props.setShow(false)}>X</button>
-            {props.children}
-            {buttons}
+        <Modal isOpen={props.isOpen} style={props.style} className='Modal' overlayClassName={buttonsNumber.length > 1? 'Overlay-no-close-button' :"Overlay"} >
+            <div className="Modal-container">
+                {buttonsNumber.length <= 1 && <div className="Modal-exit-button">
+                    <button style={styles.closeBtn} onClick={() => props.setShow(false)}>X</button>
+                </div>}
+                
+                {props.children}
+                <div className="Modal-buttons">
+                    {buttons}
+                </div>
+            </div>
         </Modal>
     )
 }

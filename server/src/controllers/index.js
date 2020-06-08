72034d1edf9ceea2ee3e98bd035c0173bc7ff508
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-06-08 16:40:33 +0300

    SETUP REPORTS CONTROLERS

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const customers = require('./customers')
 const appointments = require('./appointments')
+const reports = require('./reports')
+
 const login = require('./login');
 const error = require('./error');
 const middlewares = require('../middlewares');
@@ -20,5 +22,11 @@ router.get('/user/admin/appointment/:id', appointments.getOneAppointment)
 router.post('/user/admin/add-appointment', appointments.addOneAppointment)
 router.delete('/user/admin/delete-appointment/:id', appointments.deleteAppointment)
 router.put('/user/admin/edit-appointment/:id', appointments.updateAppointment)
+
+
+router.get('/user/admin/reports',reports.getAllReports)
+router.get('/user/admin/report/:id',reports.getSingleReport)
+router.post('/user/admin/add-report/',reports.addSingleReport)
+router.delete('/user/admin/delete-report/:id',reports.deleteReport)
 
 module.exports = router;

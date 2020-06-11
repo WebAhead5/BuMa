c512f89fc081df251f3c03bd7012ad411fb624b9
fridabu <f0504070002@gmail.com>
2020-06-11 18:24:25 +0300

    Bug, Add appointment screen, going to select customers the list is empty when it should not be empty(customers has been selected before) #85

--- client/src/screens/selectCustomer.js
+++ client/src/screens/selectCustomer.js
@@ -5,7 +5,7 @@ import CustomerSelectionList from '../components/customerSelectionList'
 import Button from '../components/Button';
 import SearchField from '../components/searchField'
 import { useRecoilValue } from 'recoil';
-import { filterDisplay, customers } from '../store/customers';
+import { filterDisplay, customers,selectedCustomers } from '../store/customers';
 import { useSetfilterDisplay, useSetCustomers, allCustomersDetailsState, filterCustomerState } from "../store/customers";
 import { fetchCustomers } from '../actions/customers';
 import { Link, useHistory } from 'react-router-dom'
@@ -28,8 +28,7 @@ const styles = {
 const SelectCustomer = () => {
     let history = useHistory();
     const allCustomers = useRecoilValue(allCustomersDetailsState);
-    const filteredCustomers = useRecoilValue(filterCustomerState)
-
+    const filteredCustomers = useRecoilValue(filterCustomerState);
 
     const setItems = useSetCustomers();
     const setFilterItems = useSetfilterDisplay();
@@ -59,13 +58,9 @@ const SelectCustomer = () => {
         }
     }
 
-
-
     const onClickGoBack = () => {
         history.goBack();  
     }
-
-
 
     return (
         <ScreenContainer>
@@ -76,17 +71,13 @@ const SelectCustomer = () => {
 
 
             <CustomerSelectionList
-
                 customersName={filteredCustomers.data}
                 error={error}
-
             />
 
             <div style={styles.btnContainer}>
 
                 <Button text="Done" onClickButton={() => onClickGoBack()} style={styles.btn} />
-                {/* <button onClick={() => onClickGoBack()} style={styles.btn}> Done </button> */}
-
 
                 <Link to={`/addCustomer`}>
 
